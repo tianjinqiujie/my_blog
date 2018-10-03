5af0ca4de728ffc750944ed9034cddfb3d45c126
qiujie <qiujie2010@qq.com>
2018-10-03 19:56:35 +0800

    个人博客

--- settings.py
+++ settings.py
@@ -21,7 +21,7 @@ class Config(object):
         host='127.0.0.1',
         port=3306,
         user='root',
-        password='qszaw123',
+        password='*',
         database='blog',
         charset='utf8'
     )
